2c4d2d9b35e248390c419e5f764e594bb50d8526
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-27 17:38:46 -0600

    add console.log

--- algo.js
+++ algo.js
@@ -5,7 +5,13 @@
 function findOdd(arr) {
     let arrDict = {};
     
-    
+    console.log("arr:", arr);
+
+    for (let idx=0; idx < arr.length; idx++) {
+        console.log(idx);
+    }
 
     return 0;
   }
+
+findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]);
